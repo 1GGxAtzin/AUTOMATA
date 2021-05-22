be3710f6b249f5b5f5529c7f1bc8789fec02f381
ATZIN NATANAEL PACHECO ARELLANO <Atzin.Pacheco@alumno.udg.mx>
2021-05-22 01:33:43 -0500

    Agregar archivos de proyecto.

---
+++ ACT TC/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace ACT_TC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tcadena = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.testados = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tresultado = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Gadugi", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(65, 101);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(148, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ingresa la Cadena:";
+            // 
+            // tcadena
+            // 
+            this.tcadena.Font = new System.Drawing.Font("Gadugi", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tcadena.Location = new System.Drawing.Point(243, 98);
+            this.tcadena.Name = "tcadena";
+            this.tcadena.Size = new System.Drawing.Size(237, 29);
+            this.tcadena.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Gadugi", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Maroon;
+            this.label2.Location = new System.Drawing.Point(413, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(200, 41);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "PROYECTO";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Gadugi", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(65, 270);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 19);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Estados:";
+            // 
+            // testados
+            // 
+            this.testados.Font = new System.Drawing.Font("Gadugi", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.testados.Location = new System.Drawing.Point(243, 270);
+            this.testados.Name = "testados";
+            this.testados.Size = new System.Drawing.Size(237, 29);
+            this.testados.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Gadugi", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(65, 179);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 19);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Resultado:";
+            // 
+            // tresultado
+            // 
+            this.tresultado.Font = new System.Drawing.Font("Gadugi", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tresultado.Location = new System.Drawing.Point(243, 179);
+            this.tresultado.Name = "tresultado";
+            this.tresultado.Size = new System.Drawing.Size(237, 29);
+            this.tresultado.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Gadugi", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(529, 97);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(84, 30);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Limpiar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(529, 159);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(84, 28);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Analizar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(984, 679);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tresultado);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.testados);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tcadena);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tcadena;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox testados;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tresultado;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ ACT TC/Form1.cs
@@ -0,0 +1,385 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ACT_TC
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+         
+            InitializeComponent();
+        }
+        List<string> cadena = new List<string>();
+        List<int> lestado = new List<int>();
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            button2.Enabled = true;
+            tcadena.Enabled = true;
+            testados.Enabled = true;
+            tresultado.Enabled = true;
+            tresultado.Text = "";
+            tcadena.Text = "";
+            testados.Text = "";
+            cadena.Clear();
+            lestado.Clear();
+            
+        }
+        void estado6(int length){
+            int i = lestado.Count();
+            for (i = lestado.Count(); i < length; ++i)
+            {
+                if (cadena[i] == "0" || cadena[i] == "1" || cadena[i] == "2" || cadena[i] == "3" || cadena[i] == "4" || cadena[i] == "5" || cadena[i] == "6" || cadena[i] == "7" || cadena[i] == "8" || cadena[i] == "9")
+                {
+                    lestado.Add(6);
+                    
+                }
+                else if(cadena[i] != "0" || cadena[i] != "1" || cadena[i] != "2" || cadena[i] != "3" || cadena[i] != "4" || cadena[i] != "5" || cadena[i] != "6" || cadena[i] != "7" || cadena[i] != "8" || cadena[i] != "9")
+                {
+                    tresultado.Text = "Error de cadena";
+                    
+                    for (i = 0; i < lestado.Count(); ++i)
+                    {
+                        testados.Text += lestado[i];
+                    }
+                    return;
+
+                }
+
+            }
+            lestado.Add(6);
+            
+            tresultado.Text = "Exponencial";
+            for (int u = 0; u < lestado.Count(); ++u)
+            {
+                testados.Text += lestado[u] + " ";
+            }
+
+
+
+
+        }
+        void estado5(int length)
+        {
+            int i = lestado.Count();
+            if (cadena[i] == "0" || cadena[i] == "1" || cadena[i] == "2" || cadena[i] == "3" || cadena[i] == "4" || cadena[i] == "5" || cadena[i] == "6" || cadena[i] == "7" || cadena[i] == "8" || cadena[i] == "9")
+            {
+                lestado.Add(5);
+                estado6(length);
+            }
+            else
+            {
+                tresultado.Text = "cadena invalida";
+                for (i = 0; i < lestado.Count(); ++i)
+                {
+                    testados.Text += lestado[i];
+                }
+            }
+
+        }
+        void estado4(int length)
+        {
+            int i = lestado.Count();
+            if (cadena[i] == "+" || cadena[i]== "-")
+            {
+                lestado.Add(4);
+                estado5(length);
+            }
+            else
+            {
+                tresultado.Text = "cadena invalida";
+                for (i = 0; i < lestado.Count(); ++i)
+                {
+                    testados.Text += lestado[i];
+                }
+            }       
+
+        }
+        void estado3(int length)
+        {
+            int j = lestado.Count(); 
+            int i = lestado.Count();
+            for (i = lestado.Count(); i < length; ++i)
+            {                             
+
+                if (cadena[i] == "0" || cadena[i] == "1" || cadena[i] == "2" || cadena[i] == "3" || cadena[i] == "4" || cadena[i] == "5" || cadena[i] == "6" || cadena[i] == "7" || cadena[i] == "8" || cadena[i] == "9")
+                {
+                    lestado.Add(3);
+                    j++;
+
+                }
+                else if (cadena[i] == "E")
+                {
+                    lestado.Add(3);
+                    estado4(length);
+                    return;
+
+                }
+            }
+            if(i == length)
+            {
+                tresultado.Text = "Numero con Decimal";
+                lestado.Add(3);
+                for (int u = 0; u < lestado.Count(); ++u)
+                {
+                    testados.Text += lestado[u] +" ";
+                }
+            }
+     
+        }
+        void estado2(int length)
+        {
+            int i = lestado.Count();
+            
+
+           if (cadena[i] == "0" || cadena[i] == "1" || cadena[i] == "2" || cadena[i] == "3" || cadena[i] == "4" || cadena[i] == "5" || cadena[i] == "6" || cadena[i] == "7" || cadena[i] == "8" || cadena[i] == "9")
+                {
+                lestado.Add(2);
+                estado3(length);
+               
+                }
+           else if (cadena[i] == "E")
+            {
+                lestado.Add(2);
+                estado4(length);
+                return;
+               
+            }
+            else 
+            {
+               
+                tresultado.Text = "Cadena incorrecta";
+
+                for (i = 0; i < lestado.Count(); ++i)
+                {
+                    testados.Text += lestado[i];
+                }
+
+            }
+            
+
+
+        }
+        void estado1(int length)
+        {
+            int j = lestado.Count();
+            int i = lestado.Count();
+            for (i = lestado.Count(); i < length; ++i)
+            {
+
+                if (cadena[i] == "0" || cadena[i] == "1" || cadena[i] == "2" || cadena[i] == "3" || cadena[i] == "4" || cadena[i] == "5" || cadena[i] == "6" || cadena[i] == "7" || cadena[i] == "8" || cadena[i] == "9")
+                {
+                    lestado.Add(1);
+                    j++;
+
+                }
+                else if(cadena[i]== ".")
+                {
+                    lestado.Add(1);
+                    estado2(length);
+                    return;
+                }
+                else
+                {
+                    tresultado.Text = "Cadena incorrecta";
+
+                    for (i = 0; i < lestado.Count(); ++i)
+                    {
+                        testados.Text += lestado[i];
+                    }
+                    return;
+
+                }
+            }
+                if (i == length)
+                {
+                    tresultado.Text = "Numero entero positivo";
+                    testados.Text = "Estado: ";
+                    for (int u = 0; u < length; ++u)
+                    {
+                        testados.Text += lestado[u] + "\n";
+                    }
+                }
+
+            
+
+        }
+        void estado7(int length)
+        {
+            int j = 1;
+            int i = 1;
+            for (i=1; i< length; ++i)
+            {
+                char character = char.Parse(cadena[i]);
+                if (Char.IsLetter(character))
+                {
+                    
+                    lestado.Add(7);
+                    j++;
+
+                }
+
+            }
+            if (j == i)
+            {
+                
+                tresultado.Text = "Cadena de caracteres (A - Z)";
+                testados.Text = "Estado: ";
+                for(int u = 0; u < length; ++u)
+                {
+                    testados.Text += lestado[u]+" ";
+                }
+            }
+            else
+            {
+               
+                tresultado.Text = "Cadena incorrecta";
+                for (i = 0; i < j-1; ++i)
+                {
+                    testados.Text += lestado[i];
+                }
+            }
+        }
+        void estado10(int length)
+        {
+            if (cadena[1] == "&" && length ==2)
+            {
+                tresultado.Text = "Operador Logico";
+                lestado.Add(10);
+                lestado.Add(8);
+                for(int i = 0; i< lestado.Count() ; ++i)
+                {
+                    testados.Text += lestado[i]+"  ";
+                }
+            }
+            else
+            {
+                tresultado.Text = "Cadena incorrecta";
+                for (int i = 0; i < lestado.Count(); ++i)
+                {
+                    testados.Text += lestado[i];
+                }
+            }
+        }
+        void estado11(int length)
+        {
+            if (cadena[1] == "|" && length == 2)
+            {
+                tresultado.Text = "Operador Logico";
+                lestado.Add(11);
+                lestado.Add(8);
+                for (int i = 0; i < lestado.Count(); ++i)
+                {
+                    testados.Text += lestado[i] + "  ";
+                }
+            }
+            else
+            {
+                tresultado.Text = "Cadena incorrecta";
+                for (int i = 0; i < lestado.Count(); ++i)
+                {
+                    testados.Text += lestado[i];
+                }
+            }
+        }
+        void estado0(int length)
+        {
+            char character = char.Parse(cadena[0]);
+            if (cadena[0] == "0" || cadena[0] == "1" || cadena[0] == "2" || cadena[0] == "3" || cadena[0] == "4" || cadena[0] == "5" || cadena[0] == "6" || cadena[0] == "7" || cadena[0] == "8" || cadena[0] == "9")
+                {
+                lestado.Add(0);
+                estado1(length);
+            }
+            
+            else if(Char.IsLetter(character)){
+                lestado.Add(0);
+                estado7(length);
+
+                
+            }
+            else if (cadena[0] == "&")
+            {
+                lestado.Add(0);
+                estado10(length);
+            }
+            else if(cadena[0]== "+" || cadena[0] == "-" || cadena[0] == "*" || cadena[0] == "/" && cadena.Count() == 1) 
+            {
+                tresultado.Text = "Operador Aritmetico";
+                lestado.Add(0);
+                lestado.Add(8);
+                for (int i = 0; i < lestado.Count(); ++i)
+                {
+                    testados.Text += lestado[i]+" ";
+                }
+
+            }
+            else if (cadena[0] == "|")
+            {
+                lestado.Add(0);
+                estado11(length);
+            }
+            else if (cadena[0] == "<" || cadena[0] == ">" || cadena[0] == "!" || cadena[0] == "=" && cadena.Count() == 2 && cadena[1]== "=")
+            {
+                tresultado.Text = "Operador Relacional";
+                lestado.Add(0);
+                lestado.Add(9);
+                lestado.Add(8);
+                for (int i = 0; i < lestado.Count(); ++i)
+                {
+                    testados.Text += lestado[i] + " ";
+                }
+
+            }
+            else
+            {
+                tresultado.Text = "Cadena invalida";
+                lestado.Add(0);
+                testados.Text = "0";
+                return;
+
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button2.Enabled = false;
+            tcadena.Enabled = false;
+            testados.Enabled = false;
+            tresultado.Enabled = false;
+            if (tcadena.Text != "")
+            {
+                
+                int length = tcadena.Text.Length;
+                string valor = tcadena.Text;
+                
+                char caracter;
+                //AGREGAR CADENA A LA LISTA
+                for (int i = 0; i < length; ++i)
+                {
+                    caracter = valor[i];
+                    cadena.Add(caracter.ToString());
+                    
+                }
+                estado0(length);
+
+            }
+            
+        }
+    }
+}
